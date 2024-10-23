5069cfdd7d4bdcbcc7372169b14df693b5892f72
Marvin Lipps <marvinlipps@googlemail.com>
2024-10-23 16:52:18 +0200

    update

---
+++ 9_Advanced_SQL/9.2.1_sql_recapitulation_challenges.sql
@@ -0,0 +1,79 @@
+USE Chinook;
+
+
+-- 1. How many artists are in the database?
+
+
+-- 2. Create an alphabetised list of the artists
+
+
+-- 3. Show only the customers from Germany.
+
+    
+-- 4. Get the full name, customer ID, and country of customers not in the US?
+
+
+-- 5. Find the track with the longest duration.
+
+
+-- 6. Which tracks have 'love' in their title?
+
+
+-- 7. What is the difference in days between the earliest and latest invoice?
+
+   
+-- 8. Which genres have more than 100 tracks?
+
+
+-- 9. Create a table showing countries alongside how many invoices there are per country
+
+
+-- 10. Find the name of the employee who has served the most customers.
+
+
+-- 11. Which customers have a first name that starts with 'A' and is 5 letters long?
+
+
+-- 12. Find the total number of tracks in each playlist.
+
+
+-- 13. Find the artist that appears in the most playlists.
+
+
+-- 14. Find the genre with the most tracks.
+
+
+-- 15. Which tracks have a composer whose name ends with 'Smith'?
+
+
+-- 16. Which artists have albums in the 'Rock' or 'Blues' genres?
+
+    
+-- 17. Which tracks are in the 'Rock' or 'Blues' genre and have a name that is exactly 5 characters long?
+
+
+-- 18. Classify customers as 'Local' if they are from Canada, 'Nearby' if they are from the USA, and 'International' otherwise.
+
+
+-- 19. Find the total invoice amount for each customer.
+
+
+-- 20. Find the customer who has spent the most on music.
+
+
+-- 21. Find the most popular album, and the associated artist.
+
+
+-- 22. How many more songs were sold from each media type?
+
+
+-- 23. Find the most popular track of 2023.
+
+
+-- 24. Find the total sales per genre, only include genres with sales between 100 and 500.
+
+
+-- 25. Find the total number of tracks sold per artist. 
+-- Add an extra column categorising the artists into 'High', 'Medium', 'Low' based on the number of tracks sold.
+-- High is more than 100, Low is less than 50.
+
---
+++ 9_Advanced_SQL/9.2.3_sql_subquery_cte_temp_table_challenges.sql
@@ -0,0 +1,34 @@
+USE chinook;
+
+-- 1. What is the difference in minutes between the total length of 'Rock' tracks and 'Jazz' tracks?
+
+
+-- 2. How many tracks have a length greater than the average track length?
+
+
+-- 3. What is the percentage of tracks sold per genre?
+
+
+-- 4. Can you check that the column of percentages adds up to 100%?
+
+
+-- 5. What is the difference between the highest number of tracks in a genre and the lowest?
+
+
+-- 6. What is the average value of Chinook customers (total spending)?
+
+
+-- 7. How many complete albums were sold? Not just tracks from an album, but the whole album bought on one invoice.
+
+
+-- 8. What is the maximum spent by a customer in each genre?
+	
+
+-- 9. What percentage of customers who made a purchase 2022 returned to make additional purchases in subsequent years?
+    
+
+-- 10. Which genre is each employee most successful at selling? Most successful is greatest amount of tracks sold.
+
+
+-- 11. How many customers made a second purchase the month after their first purchase?
+
